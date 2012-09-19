017a58c991feb606a40f360053dc9793ec22ba2a
charade <none@none>
2012-09-19 11:33:29 -0700

    test

--- lsa/laio.py
+++ lsa/laio.py
@@ -22,6 +22,17 @@ def tryIO( file, mode ):
     sys.exit(2)
   return handle
 
+def LA_Xgmml3(la_table, la_size, lsaq_table, lsaq_size, title, LA_idx=4, LS_idx=3, Delay_idx=9):
+  laq_nodes = set()
+  laq_edges = dict()   
+  lai = LA_idx-1 #4-1
+  di = Delay_idx-1 #9-1
+  li = LS_idx-1 #3-1
+
+  for i in xrange(1, laq_size+1):
+    node_x = r['''as.character''']((la_table.rx(i,True)[0]))[0]
+    node_y = r['''as.character''']((la_table.rx(i,True)[1]))[0]
+    node_z = r['''as.character''']((la_table.rx(i,True)[2]))[0]
 
 def LA_Xgmml2(la_table, la_size, lsaq_table, lsaq_size, title, LA_idx=4, LS_idx=3, Delay_idx=9):
   lsaq_nodes=set()
@@ -31,20 +42,121 @@ def LA_Xgmml2(la_table, la_size, lsaq_table, lsaq_size, title, LA_idx=4, LS_idx=
   #dict={key1:value1, k2:v2, k3:v3,....}
   #dict.remove(ki)
   #dict[kj]=vj
+  lai = LA_idx-1 #4-1
+  di = Delay_idx-1 #9-1
+  li = LS_idx-1 #3-1
   for i in xrange(1, lsaq_size+1):  
-    lsaq_edges[(node_x, node_y)]=(i, None)
-    
-  laq_nodes=lsaq_nodes
-  laq_edges=
-  
-  for i in xrnge
-    edge = laq_edges.remove((node_x, 
-    laq_edges[(node_x, node_m)] = (-1, {'LA':la_score, 'interaction':interaction_type1})  
-    laq_edges[(node_y, node_m)] = (-1, {'LA':la_score, 'interaction':interaction_type1})  
-    laq_edges[(node_z, node_m)] = (-1, {'LA':la_score, 'interaction':interaction_type1})  
-    
-  #create graph from laq_
+    node_x = r['''as.character''']((lsaq_table.rx(i,True)[0]))[0]
+    node_y = r['''as.character''']((lsaq_table.rx(i,True)[1]))[0]
+    lsaq_nodes.add(node_x)
+    lsaq_nodes.add(node_y)
+     
+    if tuple(lsaq_table.rx(i,True)[di])[0] > 0:
+         d_code = 'dr'      #direction reta
+    elif tuple(lsaq_table.rx(i,True)[di])[0] < 0:
+         d_code = 'dl'      #direction lead
+    else:
+         d_code = 'u'
+    #print(lsa_table.rx(i,True)[li])
+    if tuple(lsaq_table.rx(i,True)[li])[0] >= 0:
+         c_code = 'p'
+    else:
+         c_code = 'n'
+    interaction = c_code+d_code
+    LS_score = tuple(lsaq_table.rx(i,True)[2])[0]
+    lsaq_edges[(node_x, node_y)]=(1, {'LS':LS_score, 'interaction':interaction, 'source':node_x, 'target':node_y})
 
+  laq_nodes=lsaq_nodes
+  laq_edges=lsaq_edges
+ 
+
+  for i in xrange(1, la_size+1):
+    node_x = r['''as.character''']((la_table.rx(i,True)[0]))[0]
+    node_y = r['''as.character''']((la_table.rx(i,True)[1]))[0]
+    node_z = r['''as.character''']((la_table.rx(i,True)[2]))[0]
+    if (node_x,node_y) in laq_edges:
+      node_m_x_y = '_'.join( ['m', node_x, node_y] )
+      laq_nodes.add(node_m_x_y) 
+      laq_nodes.add(node_z)
+      if tuple(la_table.rx(i,True)[lai])[0] >= 0:
+         interaction_type3 = 'pu'
+      else:
+         interaction_type3 = 'nu'
+      if laq_edges[(node_x,node_y)][1]['interaction'] == 'pdl':
+         interaction_type1 = 'pu'
+         interaction_type2 = 'pdr'
+      elif laq_edges[(node_x,node_y)][1]['interaction'] == 'ndl':
+         interaction_type1 = 'nu'
+         interaction_type2 = 'ndr'
+      elif laq_edges[(node_x,node_y)][1]['interaction'] == 'pdr':
+         interaction_type1 = 'pdr'
+         interaction_type2 = 'pu'
+      elif laq_edges[(node_x,node_y)][1]['interaction'] == 'ndr':
+         interaction_type1 = 'ndr'
+         interaction_type2 = 'nu'
+      elif laq_edges[(node_x,node_y)][1]['interaction'] == 'pu':
+         interaction_type1 = 'pu'
+         interaction_type2 = 'pu'
+      else:
+         interaction_type1 = 'nu'
+         interaction_type2 = 'nu'
+      LA_score = tuple(la_table.rx(i,True)[3])[0]
+
+      laq_edges[(node_x, node_m_x_y)] = (-1, {'LA':LA_score, 'interaction':interaction_type1, 'source':node_x, 'target':node_m_x_y}) 
+      laq_edges[(node_y, node_m_x_y)] = (-1, {'LA':LA_score, 'interaction':interaction_type2, 'source':node_y, 'target':node_m_x_y})
+      laq_edges[(node_z, node_m_x_y)] = (-1, {'LA':LA_score, 'interaction':interaction_type3, 'source':node_z, 'target':node_m_x_y}) 
+      del laq_edges[(node_x,node_y)] 
+      
+  xgmml_element=etree.Element('graph')
+  xgmml_element.set('xmlns:dc', "http://purl.org/dc/elements/1.1/")
+  xgmml_element.set('xmlns:xlink', "http://www.w3.org/1999/xlink")
+  xgmml_element.set('xmlns:rdf', "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+  xgmml_element.set('xmlns:cy', "http://www.cytoscape.org")
+  xgmml_element.set('xmlns', "http://www.cs.rpi.edu/XGMML")
+  xgmml_element.set('directed', "1")
+  xgmml_element.set('label', "LA Network") 
+
+  for node in laq_nodes:
+    node_element = etree.SubElement(xgmml_element, 'node')
+    node_element.set('id', node)
+    node_element.set('name', node)
+    node_element.set('label', node)
+    factorName_element = etree.SubElement(node_element, 'att')
+    factorName_element.set('type', 'string')
+    factorName_element.set('name', 'factorName')
+    factorName_element.set('value', node)
+
+  for edge in laq_edges:     
+     if  laq_edges[edge][0] < 0:   
+        edge_label = '_'.join( [laq_edges[edge][1]['source'], interaction, laq_edges[edge][1]['target']] )
+        edge_element = etree.SubElement(xgmml_element, 'edge')
+        edge_element.set('label', edge_label )
+        edge_element.set('source', laq_edges[edge][1]['source'])
+        edge_element.set('target', laq_edges[edge][1]['target'])
+        interaction_element = etree.SubElement(edge_element, 'att')
+        interaction_element.set('type', 'string')
+        interaction_element.set('name', 'interaction')
+        interaction_element.set('value', laq_edges[edge][1]['interaction'])
+        LA_element = etree.SubElement(edge_element, 'att')
+        LA_element.set('type', 'real')       
+        LA_element.set('name', 'LA')
+        LA_element.set('value', "%.4f" % laq_edges[edge][1]['LA'])
+     else:
+        edge_label = '_'.join( [laq_edges[edge][1]['source'], interaction, laq_edges[edge][1]['target']] )
+        edge_element = etree.SubElement(xgmml_element, 'edge')
+        edge_element.set('label', edge_label )
+        edge_element.set('source', laq_edges[edge][1]['source'])
+        edge_element.set('target', laq_edges[edge][1]['target'])
+        interaction_element = etree.SubElement(edge_element, 'att')
+        interaction_element.set('type', 'string')
+        interaction_element.set('name', 'interaction')
+        interaction_element.set('value', laq_edges[edge][1]['interaction'])
+        LS_element = etree.SubElement(edge_element, 'att')
+        LS_element.set('type', 'real')       
+        LS_element.set('name', 'LS')
+        LS_element.set('value', "%.4f" % laq_edges[edge][1]['LS'])
+  xgmml_string = etree.tostring(xgmml_element, encoding='utf-8')
+  return xml.dom.minidom.parseString(xgmml_string).toprettyxml('  ')
 
 def LA_Xgmml(la_table, la_size, lsaq_table, lsaq_size, title, LA_idx=4, LS_idx=3, Delay_idx=9):
 
