fix errors in trend p-value calculation and add ptheo, vtrend for simple trend variance and theoretical pvalue calculations

lsa/lsa_sim.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 
-#This simple script generates two series of n iid Normal
-#input: Number of Simulations (S), Length of Sequence (L), Delay Limit (D), Output Filename (F)
-#output: R
-
 import argparse, time, sys
 import numpy as np
 
 #kcut_min=100
 #Rmax_min=10
 my_decimal = 3    # preset x step size for P_table
-#pipi = np.pi**2 # pi^2
-#pipi_inv = 1/pipi
 
 try:
   #debug import
@@ -20,31 +14,6 @@ try:
 except ImportError:
   #installed import
   from lsa import lsalib
-
-#def ji_calc_trend(oSeries, thresh):
-  #Liping Ji and Kian-Lee Tan, Bioinformatics 2005
-  
-#  lengthSeries=oSeries.shape[1]-1
-#  tSeries = np.zeros((1,lengthSeries), dtype='float')
-
-#  for i in xrange(0, lengthSeries):
-#    if oSeries[0][i] == 0 and oSeries[0][i+1] > 0:
-#      trend = 1
-#    elif oSeries[0][i] == 0 and oSeries[0][i+1] < 0:
-#      trend = -1
-#    elif oSeries[0][i] == 0 and oSeries[0][i+1] == 0:
-#      trend = 0
-#    else:
-#      trend = (oSeries[0][i+1]-oSeries[0][i])/np.abs(oSeries[0][i])
-
-#    if trend >= thresh:
-#      tSeries[0][i] = 1
-#    elif trend <= -thresh:
-#      tSeries[0][i] = -1
-#    else:
-#      tSeries[0][i] = 0
-
-#  return tSeries
 
 def main():
 
@@ -56,7 +25,8 @@ def main():
       help="specify the times of simulation to run, default: 10000")
   parser.add_argument("-D", "--delayLimit", dest="delayLimit", default=0, type=int,\
       help="specify the maximum delay possible, default: 0")
-  parser.add_argument("-L", "--lengthSeries", dest="lengthSeries", default=50, type=int,\
+  parser.add_argument("-L", "--lengthSeries", dest="lengthSeries", \
+      default=50, type=int,\
       help="specify the length of series to generate, default: 50")
   parser.add_argument("-T", "--trendSeries", dest="trendSeries", default=None,\
       help="if specified must be a threshold number, will generate trend series, \
@@ -72,9 +42,11 @@ def main():
   parser.add_argument("-n", "--nullDistribution", dest="nullDistribution", \
       default="yes", choices=['yes','no'],
       help="yes: randomize timpoints; no: do not randomize timepoints")
-  parser.add_argument("-P", "--permPrecision", dest="permPrecision", default=1, type=float,\
+  parser.add_argument("-P", "--permPrecision", dest="permPrecision", \
+      default=1, type=float,\
       help="numeric>0, default=.1, inverse of number of permutations")
-  parser.add_argument("-x", "--theoPrecision", dest="theoPrecision", default=0.1, type=float,\
+  parser.add_argument("-x", "--theoPrecision", dest="theoPrecision", \
+      default=0.1, type=float,\
       help="numeric>0, default=0.1, precision of theo approximation" )
   parser.add_argument("-d", "--simDelay", dest="simDelay", default=0, type=int,\
       help="sim delay effects" )
@@ -95,7 +67,7 @@ def main():
   nullDistribution = vars(arg_namespace)['nullDistribution']
   #theo_precision = 0.0001
 
-  if not vars(arg_namespace)['trendSeries']:
+  if vars(arg_namespace)['trendSeries'] == None:
     trendSeries = False
     x_var = approxVar
   else:
@@ -133,33 +105,34 @@ def main():
       nnz_x = lengthSeries - nz_x + 1
       nnz_y = lengthSeries - nz_y + 1
 
+    #generate two arrays: OxSeries, OySeries
     if simMethod[0] == 'idn':
-      OxSeries = np.random.randn(1,nnz_x)
-      OySeries = np.random.randn(1,nnz_y)
-    elif simMethod[0] == 'sin':
+      OxSeries = np.random.randn(1,nnz_x)[0]
+      OySeries = np.random.randn(1,nnz_y)[0]
+    elif simMethod[0] == 'sin':  # to implement
       sigma = float(simMethod[1])
       x = np.array(range(0,nnz_x+simDelay), dtype='float')
       x.shape = (1,len(x))
       sin_x = np.sin(x*np.pi/6.)
       OxSeries = sin_x[:nnz_x+1]
       OySeries = sin_x[simDelay:nnz_x+simDelay+1] + np.random.randn(1,nnz_x)
-    else:
+    else: # to implement
       print >>sys.stderr, "simMethod not implemented"
 
-    #OxSeries = OxSeries[0]
-    #OySeries = OySeries[0]
+    lengthXSeries=len(OxSeries)
+    lengthYSeries=len(OySeries)
     if not trendSeries:
       xSeries = OxSeries
       ySeries = OySeries
     else:
-      #lengthXSeries=len(OxSeries)
-      #lengthYSeries=len(OySeries)
-      xSeries = lsalib.ji_calc_trend_rep(OxSeries, trend_threshold)
-      ySeries = lsalib.ji_calc_trend_rep(OySeries, trend_threshold)
+      xSeries = lsalib.ji_calc_trend(OxSeries, lengthXSeries, trend_threshold)
+      ySeries = lsalib.ji_calc_trend(OySeries, lengthYSeries, trend_threshold)
 
     if nullDistribution == 'yes':
-      xSeries = np.array([np.random.permutation(np.concatenate((xSeries[0],np.zeros(nz_x))))])
-      ySeries = np.array([np.random.permutation(np.concatenate((ySeries[0],np.zeros(nz_y))))])
+      xSeries = np.array([np.random.permutation\
+          (np.concatenate((xSeries,np.zeros(nz_x))))])
+      ySeries = np.array([np.random.permutation\
+          (np.concatenate((ySeries,np.zeros(nz_y))))])
 
     xSeries = np.ma.masked_invalid(xSeries)
     ySeries = np.ma.masked_invalid(ySeries)
@@ -170,17 +143,21 @@ def main():
     (alpha[j], beta[j]) = (a,b)
 
     if not trendSeries:
-      lsa_result = lsalib.singleLSA(xSeries, ySeries, delayLimit, lsalib.simpleAverage, \
-          lsalib.percentileZNormalize, True)
+      xSeriesData = xSeries.reshape(1,lengthSeries)
+      ySeriesData = ySeries.reshape(1,lengthSeries) 
+      lsa_result = lsalib.singleLSA(xSeriesData, ySeriesData, delayLimit, \
+          lsalib.simpleAverage, lsalib.percentileZNormalize, True)
       LS_values[j] = np.abs( lengthSeries * lsa_result.score )
-      P_perm[j] = lsalib.permuPvalue(xSeries, ySeries, delayLimit, int(1/perm_precision), \
-          LS_values[j]/lengthSeries, lsalib.simpleAverage, lsalib.percentileZNormalize)
+      P_perm[j] = lsalib.permuPvalue(xSeriesData, ySeriesData, delayLimit, \
+          int(1/perm_precision), LS_values[j]/lengthSeries, \
+          lsalib.simpleAverage, lsalib.percentileZNormalize)
     else:
-      lsa_result = lsalib.singleLSA(xSeries, ySeries, delayLimit, lsalib.simpleAverage, \
-          lsalib.noneNormalize, True)
+      lsa_result = lsalib.singleLSA(xSeriesData, ySeriesData, delayLimit, \
+          lsalib.simpleAverage, lsalib.noneNormalize, True)
       LS_values[j] = np.abs( lengthSeries * lsa_result.score )
-      P_perm[j] = lsalib.permuPvalue(xSeries, ySeries, delayLimit, int(1/perm_precision), \
-          LS_values[j]/lengthSeries, lsalib.simpleAverage, lsalib.noneNormalize)
+      P_perm[j] = lsalib.permuPvalue(xSeriesData, ySeriesData, delayLimit, \
+          int(1/perm_precision), LS_values[j]/lengthSeries, \
+          lsalib.simpleAverage, lsalib.noneNormalize)
     P_theo[j] = lsalib.readPvalue(P_table, R=LS_values[j], N=lengthSeries, \
         x_sd=np.sqrt(approxVar), M=1, alpha=a, beta=b, x_decimal=my_decimal)
     Al_tmp = len(lsa_result.trace)
@@ -190,12 +167,12 @@ def main():
   print >>resultFile, "R\tP_theo\tP_perm\talpha\tbeta\tu1\tu2\tv1\tv2\tXs\tYs\tD\tAl"
   print >>resultFile, '\n'.join(['\t'.join( \
     [str(LS_values[i]),str(P_theo[i]),str(P_perm[i]),str(alpha[i]),str(beta[i]),\
-    str(u1[i]),str(u2[i]),str(v1[i]),str(v2[i]),str(Xs[i]),str(Ys[i]),str(D[i]),str(Al[i])]
+    str(u1[i]),str(u2[i]),str(v1[i]),str(v2[i]),\
+    str(Xs[i]),str(Ys[i]),str(D[i]),str(Al[i])]\
     ) for i in xrange(0,simTimes)])
 
   end_time = time.time()
   elapse_time = end_time - start_time
-
   print >>sys.stderr, "finished in %d seconds" % elapse_time
 
 if __name__=="__main__":

lsa/lsalib.py
@@ -298,7 +298,7 @@ def readPvalue(P_table, R, N, x_sd=1, M=1, alpha=1, beta=1, x_decimal=3):
   # x' = R*M/(alpha*beta*sqrt(N)*sd) * 10^(x_decimal)
   # has to ceil the x value to avoid round to 0, which is not amenable to calculation
   try:
-    xi = int(np.around(R*M/np.sqrt(alpha*beta*N)*(10**x_decimal)))    #
+    xi = int(np.around(R*M/(x_sd*np.sqrt(alpha*beta*N))*(10**x_decimal)))    #
   except OverflowError:
     #print alpha, beta
     #print "x=", np.around(R*M/np.sqrt(alpha*beta*N)*(10**x_decimal))
@@ -309,11 +309,15 @@ def readPvalue(P_table, R, N, x_sd=1, M=1, alpha=1, beta=1, x_decimal=3):
   else:
     return 0.
 
-def theoPvalue(Rmax, Dmax=0, precision=.001, x_decimal=2):   #let's produce 2 tail-ed p-value
-  # the produced P_table is for P(X>=x) when X=(R(D)/sqrt(n)) and indexed by xi=x*(10^x_decimal) 
+def theoPvalue(Rmax, Dmax=0, precision=.001, x_decimal=2):   
+  # let's produce 2 tail-ed p-value
+  # the produced P_table is for P(X>=x) when X=(R(D)/sqrt(n)) 
+  # and indexed by xi=x*(10^x_decimal) 
   # so, x=xi/(10^x_decimal)
-  # no read for recaculating P_table when read proper scaled x' = R*M/(alpha*beta*sqrt(N)*sd) * 10^(x_decimal)
-  # print np.linspace(0,timespots,timespots/10**(-x_decimal)+1) #x_decimal is for augment x_index
+  # no read for recaculating P_table when read proper scaled 
+  # x' = R*M/(alpha*beta*sqrt(N)*sd) * 10^(x_decimal)
+  # print np.linspace(0,timespots,timespots/10**(-x_decimal)+1) 
+  # x_decimal is for augment x_index
   Rmax = np.max((Rmax, Rmax_min))
   P_table = dict()
   for xi in xrange(0,Rmax*10**(x_decimal)+1): 
@@ -352,7 +356,8 @@ def theoPvalue(Rmax, Dmax=0, precision=.001, x_decimal=2):   #let's produce 2 ta
 
   return P_table
 	
-def permuPvalue(series1, series2, delayLimit, precisionP, Smax, fTransform, zNormalize):
+def permuPvalue(series1, series2, delayLimit, precisionP, \
+    Smax, fTransform, zNormalize):
   """ do permutation Test
 
     Args:
@@ -808,8 +813,10 @@ def fillMissing(tseries, method): #teseries is 2d matrix unmasked
 
     return yy
     
-def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5, bootCI=.95, bootNum=0, pvalueMethod='perm', precisionP=1000,\
-    fTransform=simpleAverage, zNormalize=noZeroNormalize, varianceX=1, resultFile=open("tmp.lsa","w"), \
+def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5, \
+    bootCI=.95, bootNum=0, pvalueMethod='perm', precisionP=1000,\
+    fTransform=simpleAverage, zNormalize=noZeroNormalize, varianceX=1, \
+    resultFile=tempfile.TemporaryFile('w'), \
     firstFactorLabels=None, secondFactorLabels=None, qvalueMethod='R'):
   """ calculate pairwise LS scores and p-values
 
@@ -833,8 +840,9 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
         	
   """	
 
-  col_labels= ['X','Y','LS','lowCI','upCI','Xs','Ys','Len','Delay','P','PCC','Ppcc','SPCC','Pspcc','Dspcc','SCC','Pscc','SSCC','Psscc','Dsscc',
-            'Q','Qpcc','Qspcc','Qscc','Qsscc','Xi','Yi']
+  col_labels= ['X','Y','LS','lowCI','upCI','Xs','Ys','Len','Delay',\
+      'P','PCC','Ppcc','SPCC','Pspcc','Dspcc','SCC','Pscc','SSCC',\
+      'Psscc','Dsscc','Q','Qpcc','Qspcc','Qscc','Qsscc','Xi','Yi']
   print >>resultFile,  "\t".join(col_labels)
 
   firstFactorNum = firstData.shape[0]
@@ -876,18 +884,21 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
 
   if pvalueMethod in ['theo','mix']:
     #P_table = theoPvalue(D=0, precision=.0001, x_decimal=3)   #let's produce 2 tail-ed p-value
-    P_table = theoPvalue(Rmax=timespots, Dmax=delayLimit, precision=1./precisionP, x_decimal=my_decimal)
+    P_table = theoPvalue(Rmax=timespots, Dmax=delayLimit, \
+        precision=1./precisionP, x_decimal=my_decimal)
     #print P_table
   for i in xrange(0, firstFactorNum):
     Xz = np.ma.masked_invalid(firstData[i], copy=True) #need to convert to masked array with na's, not F-normalized
-    Xz_badOccur = np.sum(np.logical_not(np.isnan(ma_average(Xz)), ma_average(Xz)==0))/float(timespots) < minOccur
+    Xz_badOccur = np.sum(np.logical_not(np.isnan(ma_average(Xz)), \
+        ma_average(Xz)==0))/float(timespots) < minOccur
     if Xz.shape[1] == None: #For 1-d array, convert to 2-d
       Xz.shape = (1, Xz.shape[0])
     for j in xrange(0, secondFactorNum):
       if onDiag and i>=j:
         continue   #only care lower triangle entries, ignore i=j entries
       Yz = np.ma.masked_invalid(secondData[j], copy=True)    # need to convert to masked array with na's, not F-normalized
-      Yz_badOccur = np.sum(np.logical_not(np.isnan(ma_average(Yz)), ma_average(Yz)==0))/float(timespots) < minOccur
+      Yz_badOccur = np.sum(np.logical_not(np.isnan(ma_average(Yz)), \
+          ma_average(Yz)==0))/float(timespots) < minOccur
       #print i+1, np.sum(np.logical_not(np.isnan(ma_average(Xz)), ma_average(Xz)==0))/float(timespots), Xz_badOccur, \
       #	    j+1, np.sum(np.logical_not(np.isnan(ma_average(Yz)), ma_average(Yz)==0))/float(timespots), Yz_badOccur
       #if Yz_badOccur:
@@ -904,7 +915,9 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
       
       if np.all(Yz.mask) or np.all(Xz.mask) or Xz_badOccur or Yz_badOccur:  # not any unmasked value in Xz or Yz, all nan in input, warn code -1
       #lsaTable[ti] = [i, j, Smax,   Sl,     Su,     Xs,Ys,Al,Xs-Ys, lsaP,   PCC,    P_PCC,  SPCC,   P_SPCC, D_SPCC, SCC,    P_SCC,  SSCC,   P_SSCC, D_SSCC]
-        lsaTable[ti] =[i, j, np.nan, np.nan, np.nan, -1, -1, -1, -1, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+        lsaTable[ti] =[i, j, np.nan, np.nan, np.nan, -1, -1, -1, -1, \
+            np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, \
+            np.nan, np.nan, np.nan, np.nan]
         pvalues[ti] = np.nan
         pccpvalues[ti] = np.nan
         spccpvalues[ti] = np.nan
@@ -926,13 +939,16 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
           
       Smax = LSA_result.score                                                               # get Smax
       Al = len(LSA_result.trace)
-      (PCC, P_PCC) = calc_pearsonr(ma_average(Xz, axis=0), ma_average(Yz, axis=0)) # two tailed p-value
+      (PCC, P_PCC) = calc_pearsonr(ma_average(Xz, axis=0), ma_average(Yz, axis=0)) 
+      # it is two tailed p-value
       (SCC, P_SCC) = calc_spearmanr(ma_average(Xz, axis=0), ma_average(Yz, axis=0))
       
       try:
-        (SPCC, P_SPCC, D_SPCC) = calc_shift_corr(ma_average(Xz, axis=0), ma_average(Yz, axis=0), delayLimit, calc_pearsonr) 
+        (SPCC, P_SPCC, D_SPCC) = calc_shift_corr(ma_average(Xz, axis=0), \
+            ma_average(Yz, axis=0), delayLimit, calc_pearsonr) 
         # corr for shifted-cut seq
-        (SSCC, P_SSCC, D_SSCC) = calc_shift_corr(ma_average(Xz, axis=0), ma_average(Yz, axis=0), delayLimit, calc_spearmanr) 
+        (SSCC, P_SSCC, D_SSCC) = calc_shift_corr(ma_average(Xz, axis=0), \
+            ma_average(Yz, axis=0), delayLimit, calc_spearmanr) 
         # corr for shifted-cut seq
         #if np.isnan(SSCC) or np.isnan(SPCC): 
         #  print "Al=", Al, "X shape", Xz.shape, "Y shape", Yz.shape
@@ -958,11 +974,14 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
         #(SCC, P_SCC) = calc_spearmanr(ma_average(Xz, axis=0), ma_average(Yz, axis=0), statlib.stats.lspearmanr)
         pvalues[ti] = np.nan
         #lsaTable[ti] = [i, j, Smax,   Sl,     Su,     Xs,Ys,Al,Xs-Ys, lsaP,   PCC, P_PCC,  SPCC,   P_SPCC, D_SPCC, SCC, P_SCC,  SSCC, P_SSCC, D_SSCC]
-        lsaTable[ti] =  [i, j, np.nan, np.nan, np.nan, 0, 0, 0, 0,     np.nan, PCC, P_PCC,  SPCC, P_SPCC, D_SPCC, SCC, P_SCC, SSCC, P_SSCC, D_SSCC]
+        lsaTable[ti] =  [i, j, np.nan, np.nan, np.nan, 0, 0, 0, 0,\
+            np.nan, PCC, P_PCC,  SPCC, P_SPCC, D_SPCC, \
+            SCC, P_SCC, SSCC, P_SSCC, D_SSCC]
         ti += 1
         continue
 
-      (Xs, Ys, Al) = (LSA_result.trace[Al-1][0], LSA_result.trace[Al-1][1], len(LSA_result.trace))
+      (Xs, Ys, Al) = (LSA_result.trace[Al-1][0], \
+          LSA_result.trace[Al-1][1], len(LSA_result.trace))
       #try:
       #  (Xs, Ys, Al) = (LSA_result.trace[Al-1][0], LSA_result.trace[Al-1][1], len(LSA_result.trace))
       #except IndexError:
@@ -1000,19 +1019,23 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
         if Xz_alpha==0 or Yz_beta==0:
           lsaP = np.nan
         else:
-          lsaP = readPvalue(P_table, R=np.abs(Smax)*timespots, N=timespots, x_sd=stdX, M=replicates, alpha=Xz_alpha, beta=Yz_beta, x_decimal=my_decimal)
+          lsaP = readPvalue(P_table, R=np.abs(Smax)*timespots, \
+            N=timespots, x_sd=stdX, M=replicates, alpha=Xz_alpha, \
+            beta=Yz_beta, x_decimal=my_decimal)
 
       if (pvalueMethod in ['mix'] and lsaP<=promisingP) or (pvalueMethod in ['perm']):
         Xp = np.ma.array(Xz,copy=True)
         Yp = np.ma.array(Yz,copy=True)
-        lsaP = permuPvalue(Xp, Yp, delayLimit, precisionP, np.abs(Smax), fTransform, zNormalize)          # do Permutation Test
+        lsaP = permuPvalue(Xp, Yp, delayLimit, precisionP, np.abs(Smax), \
+            fTransform, zNormalize)          # do Permutation Test
 
       pvalues[ti] = lsaP
       #print "bootstrap computing..."
       if bootNum > 0: #do BS
         Xb = np.ma.array(Xz,copy=True)
         Yb = np.ma.array(Yz,copy=True)
-        (Smax, Sl, Su) = bootstrapCI(Xb, Yb, Smax, delayLimit, bootCI, bootNum, fTransform, zNormalize)           # do Bootstrap CI
+        (Smax, Sl, Su) = bootstrapCI(Xb, Yb, Smax, delayLimit, \
+            bootCI, bootNum, fTransform, zNormalize)           # do Bootstrap CI
       else: #skip BS
         (Smax, Sl, Su) = (Smax, Smax, Smax)
 
@@ -1025,7 +1048,8 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
       #quit()
 
       # need +epsilon to avoid all zeros
-      lsaTable[ti] = [i, j, Smax, Sl, Su, Xs, Ys, Al, Xs-Ys, lsaP, PCC, P_PCC, SPCC, P_SPCC, D_SPCC, SCC, P_SCC, SSCC, P_SSCC, D_SSCC]
+      lsaTable[ti] = [i, j, Smax, Sl, Su, Xs, Ys, Al, Xs-Ys, lsaP, PCC, P_PCC, \
+          SPCC, P_SPCC, D_SPCC, SCC, P_SCC, SSCC, P_SSCC, D_SSCC]
       ti += 1
       del LSA_result
       #print "finalizing..."
@@ -1067,8 +1091,9 @@ def applyAnalysis(firstData, secondData, onDiag=True, delayLimit=3, minOccur=.5,
   #print lsaTable
   for row in lsaTable:
     print >>resultFile, "\t".join(['%s']*len(col_labels)) % \
-      tuple( [firstFactorLabels[row[0]], secondFactorLabels[row[1]]]  
-          + ["%f" % np.round(v, decimals=disp_decimal) if isinstance(v, float) else v for v in row[2:]]
+      tuple( [firstFactorLabels[row[0]], secondFactorLabels[row[1]]] \
+          + ["%f" % np.round(v, decimals=disp_decimal) \
+              if isinstance(v, float) else v for v in row[2:]] \
           + [row[0]+1, row[1]+1] )
 
 #### trend analysis functions ####
@@ -1139,6 +1164,10 @@ def float_equal(a,b,tol=0.000001):
     return True
   else:
     return False
+
+def calc_markov_var(P):
+  v = 4*((-P[3]/(-1+P[1]+P[2]-2*P[3]))**2)*(1+2*((P[1]-P[2])**2)/(1-((P[1]-P[2])**2)))
+  return v
 
 #default float_tol = 6 decimal
 def calc_eigen(P):